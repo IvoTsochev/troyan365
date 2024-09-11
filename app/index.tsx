549c790579fc09527613386c6e53bdbf81e7bbec
Ivaylo Tsochev <ivo.tsochev@live.com>
2024-09-11 12:15:41 +0300

    Packages and config
    - installing packages and configuration setting up
    - nativewind set up

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const App = () => {
+  return (
+    <View className="w-full h-full justify-center items-center">
+      <Text className="text-8xl font-extrabold">index</Text>
+    </View>
+  );
+};
+
+export default App;
