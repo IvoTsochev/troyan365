824b25fca1243655895af1aba0014fd9b5c29da4
Ivaylo Tsochev <ivo.tsochev@live.com>
2024-11-23 14:55:02 +0200

    86c11mhzd Package Update V2
    - fixing font styling

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -92,7 +92,7 @@ const Home = () => {
           <View className="mt-6 px-4 space-y-6">
             <View className="justify-between items-start flex-row mb-6">
               <View>
-                <Text className="font-pmedium text-sm text-gray-100 py-3">
+                <Text className="font-pbold text-lg text-gray-100 py-3">
                   Добре дошъл
                 </Text>
                 {userData?.username && (
